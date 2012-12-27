b764b831617ce2dbd9e9e4d8a01152c2973a4c8a
Grégoire Barbier <devel@g76r.eu>
2012-12-28 00:18:56 +0100

    added support for HTML classes, links and prefixes in HtmlTableView

--- textview/htmltableview.cpp
+++ textview/htmltableview.cpp
@@ -1,27 +1,11 @@
 #include "htmltableview.h"
 #include <QtDebug>
 
-HtmlTableView::HtmlTableView(QObject *parent) : AsyncTextView(parent) {
+HtmlTableView::HtmlTableView(QObject *parent) : AsyncTextView(parent),
+  _thClassRole(-1), _trClassRole(-1), _tdClassRole(-1), _linkRole(-1),
+  _linkClassRole(-1), _htmlPrefixRole(-1) {
   //qDebug() << "HtmlTableView()" << parent;
 }
-
-/*inline void writeHtmlUlTree(QAbstractItemModel *m, QString &v,
-                            QModelIndex parent, int depth) {
-  v.append("<ul>");
-  int rows = m->rowCount(parent);
-  int columns = m->columnCount(parent);
-  qDebug() << "TreeHtmlView::writeHtmlTree()" << depth << parent << rows
-           << columns;
-  for (int row = 0; row < rows; ++row) {
-    v.append("<li>");
-    for (int column = 0; column < columns; ++column) {
-      QModelIndex index = m->index(row, column, parent);
-      v.append(m->data(index).toString()).append(" ");
-    }
-    writeHtmlUlTree(m, v, m->index(row, 0, parent), depth+1);
-  }
-  v.append("</ul>");
-}*/
 
 void HtmlTableView::writeHtmlTableTree(QAbstractItemModel *m, QString &v,
                                        QModelIndex parent, int depth) {
@@ -30,15 +14,41 @@ void HtmlTableView::writeHtmlTableTree(QAbstractItemModel *m, QString &v,
   //qDebug() << "HtmlTableView::writeHtmlTableTree()" << depth << parent << rows
   //         << columns;
   for (int row = 0; row < rows; ++row) {
-    v.append("<tr>");
+    if (_trClassRole >= 0)
+      v.append("<tr class=\"")
+          .append(m->data(m->index(row, 0, parent), _trClassRole).toString())
+          .append("\">");
+    else
+      v.append("<tr>");
     for (int column = 0; column < columns; ++column) {
-      v.append("<td>");
+      QModelIndex index = m->index(row, column, parent);
+      if (_tdClassRole >= 0)
+        v.append("<td class=\"")
+            .append(m->data(index, _trClassRole).toString())
+            .append("\">");
+      else
+        v.append("<td>");
       if (!column) {
         for (int i = 0; i < depth; ++i)
           v.append("&nbsp;&nbsp;");
       }
-      QModelIndex index = m->index(row, column, parent);
+      if (_htmlPrefixRole >= 0)
+        v.append(m->data(index, _htmlPrefixRole).toString());
+      QString link;
+      if (_linkRole >= 0)
+        link = m->data(index, _linkRole).toString();
+      if (!link.isEmpty()) {
+        v.append("<a href=\"").append(link);
+        if (_linkClassRole >= 0)
+          v.append("\" class=\"")
+              .append(m->data(index, _linkClassRole).toString())
+              .append("\">");
+        else
+          v.append("\">");
+      }
       v.append(m->data(index).toString());
+      if (!link.isEmpty())
+        v.append("</a>");
       v.append("</td>");
     }
     v.append("</tr>\n");
@@ -50,7 +60,10 @@ void HtmlTableView::updateText() {
   QAbstractItemModel *m = model();
   QString v;
   if (m) {
-    v.append("<table>\n<tr>");
+    if (_tableClass.isEmpty())
+      v.append("<table>\n<tr>");
+    else
+      v.append(QString("<table class=\"%1\">\n<tr>").arg(_tableClass));
     int columns = m->columnCount(QModelIndex());
     for (int i = 0; i < columns; ++i)
       v.append("<th>").append(m->headerData(i, Qt::Horizontal).toString())
--- textview/htmltableview.h
+++ textview/htmltableview.h
@@ -6,11 +6,25 @@
 /** Display the model content as a HTML table which first column is
  * indented to reflect the tree of the model if any.
  */
-// LATER add style options (headers or not, html classes, icons, indentation string, columns selection, hide non-leaf rows...)
+// LATER add style options (headers or not, indentation string, columns selection, hide non-leaf rows...)
+// LATER implement thClassRole and tdClassRole for real
 class LIBQTSSUSHARED_EXPORT HtmlTableView : public AsyncTextView {
   Q_OBJECT
+  QString _tableClass;
+  int _thClassRole, _trClassRole, _tdClassRole, _linkRole, _linkClassRole;
+  int _htmlPrefixRole;
+
 public:
   explicit HtmlTableView(QObject *parent = 0);
+  void setTableClass(const QString tableClass) { _tableClass = tableClass; }
+  void setThClassRole(int role) { _thClassRole = role; }
+  void setTrClassRole(int role) { _trClassRole = role; }
+  void setTdClassRole(int role) { _tdClassRole = role; }
+  /** Surround Qt::DisplayRole text with <a href="${linkRole}"> and </a>. */
+  void setLinkRole(int role) { _linkRole = role; }
+  void setLinkClassRole(int role) { _linkClassRole = role; }
+  /** Prefix with unescaped HTML text, e.g. "<img src='icon/foo.png'/>". */
+  void setHtmlPrefixRole(int role) { _htmlPrefixRole = role; }
 
 protected:
   void updateText();
