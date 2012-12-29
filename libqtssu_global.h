7089946ee2afb67b1c6bcc92ec1ba457bf081179
Grégoire Barbier <devel@g76r.eu>
2012-12-29 16:08:00 +0100

    Fixed license (switched to AGPL and fixed/added header in every file)

--- libqtssu_global.h
+++ libqtssu_global.h
@@ -1,17 +1,16 @@
-/*
-Copyright 2012 Hallowyn and others.
-See the NOTICE file distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file to you under
-the Apache License, Version 2.0 (the "License"); you may not use this
-file except in compliance with the License. You may obtain a copy of the
-License at http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
-License for the specific language governing permissions and limitations
-under the License.
-*/
-
+/* Copyright 2012 Hallowyn and others.
+ * This file is part of libqtssu, see <https://github.com/g76r/libqtssu>.
+ * Libqtssu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Libqtssu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with libqtssu.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef LIBQTSSU_GLOBAL_H
 #define LIBQTSSU_GLOBAL_H
 
--- textview/viewscomposerhandler.cpp
+++ textview/viewscomposerhandler.cpp
@@ -1,3 +1,16 @@
+/* Copyright 2012 Hallowyn and others.
+ * This file is part of libqtssu, see <https://github.com/g76r/libqtssu>.
+ * Libqtssu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Libqtssu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with libqtssu.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "viewscomposerhandler.h"
 
 ViewsComposerHandler::ViewsComposerHandler(QObject *parent,
