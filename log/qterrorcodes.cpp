e887ea8b7d0beebb11d9971a1b3ae5ba2ae4bf1c
Grégoire Barbier <devel@g76r.eu>
2014-08-08 09:53:18 +0200

    copyright notice fix

--- log/qterrorcodes.cpp
+++ log/qterrorcodes.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2013 Hallowyn and others.
+/* Copyright 2013-2014 Hallowyn and others.
  * This file is part of qron, see <http://qron.hallowyn.com/>.
  * Qron is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
