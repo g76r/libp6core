ea65df670fd2867deb556a6c62eb20e23a65c3ea
Grégoire Barbier <devel@g76r.eu>
2014-11-17 22:13:12 +0100

    fixed header copyright notices

--- log/filelogger.h
+++ log/filelogger.h
@@ -1,15 +1,15 @@
 /* Copyright 2012-2014 Hallowyn and others.
- * This file is part of qron, see <http://qron.hallowyn.com/>.
- * Qron is free software: you can redistribute it and/or modify
+ * This file is part of libqtssu, see <https://github.com/g76r/libqtssu>.
+ * Libqtssu is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * Qron is distributed in the hope that it will be useful,
+ * Libqtssu is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  * You should have received a copy of the GNU Affero General Public License
- * along with qron. If not, see <http://www.gnu.org/licenses/>.
+ * along with libqtssu.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef FILELOGGER_H
 #define FILELOGGER_H
