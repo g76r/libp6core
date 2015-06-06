g76r/libp6core

commit cbd2bfec7e7fcd2010c5ea9edce1fecd1435fe3d

paramset+log: performance improvements

log/log.h
@@ -152,8 +152,6 @@ public:
     _message.append(o); return *this; }
   inline LogHelper &operator<<(const QChar &o) {
     _message.append(o); return *this; }
-  inline LogHelper &operator<<(const char *o) {
-    _message.append(o); return *this; }
   inline LogHelper &operator<<(char o) {
     _message.append(o); return *this; }
   inline LogHelper &operator<<(qint64 o) {
@@ -172,28 +170,25 @@ public:
   inline LogHelper &operator<<(const QVariant &o) {
     _message.append(o.toString()); return *this; }
   inline LogHelper &operator<<(const QList<QString> &o) {
-    _message.append(QStringLiteral("{ "));
+    _message.append("{ ");
     foreach (const QString &s, o)
-      _message.append(QStringLiteral("\"")).append(s)
-          .append(QStringLiteral("\" "));
-    _message.append(QStringLiteral("}"));
+      _message.append("\"").append(s).append("\" ");
+    _message.append("}");
     return *this; }
   inline LogHelper &operator<<(const QSet<QString> &o) {
-    _message.append(QStringLiteral("{ "));
+    _message.append("{ ");
     foreach (const QString &s, o)
-      _message.append(QStringLiteral("\"")).append(s)
-          .append(QStringLiteral("\" "));
-    _message.append(QStringLiteral("}"));
+      _message.append("\"").append(s).append("\" ");
+    _message.append("}");
     return *this; }
   inline LogHelper &operator<<(const QObject *o) {
     const QMetaObject *mo = o ? o->metaObject() : 0;
     if (mo)
-      _message.append(mo->className()).append(QStringLiteral("(0x"))
+      _message.append(mo->className()).append("(0x")
           .append(QString::number(reinterpret_cast<qintptr>(o), 16))
-          .append(QStringLiteral(", \"")).append(o->objectName())
-          .append(QStringLiteral("\")"));
+          .append(", \"").append(o->objectName()).append("\")");
     else
-      _message.append(QStringLiteral("QObject(0x0)"));
+      _message.append("QObject(0x0)");
     return *this; }
   inline LogHelper &operator<<(const QObject &o) {
     return operator<<(&o); }

util/paramset.cpp
@@ -373,10 +373,12 @@ QPair<QString,QString> ParamSet::valueAsStringsPair(
                                 evaluate(v.mid(i+1).trimmed(), context));
 }
 
+static const char matchingPatternSpecialChars[] = "*?[]\\";
+#define CONTAINS(array, item) (::memchr((array), (item), sizeof(array)/sizeof(*(array))))
+
 QString ParamSet::matchingPattern(QString rawValue) {
   int i = 0;
   QString value, variable;
-  QString specialChars("*?[]\\");
   while (i < rawValue.size()) {
     QChar c = rawValue.at(i++);
     if (c == '%') {
@@ -407,10 +409,12 @@ QString ParamSet::matchingPattern(QString rawValue) {
         }
         value.append("*");
       } else {
-        value.append(specialChars.contains(c) ? '?' : c);
+        value.append(CONTAINS(matchingPatternSpecialChars, c.toLatin1())
+                     ? '?' : c);
       }
     } else {
-      value.append(specialChars.contains(c) ? '?' : c);
+      value.append(CONTAINS(matchingPatternSpecialChars, c.toLatin1())
+                   ? '?' : c);
     }
   }
   return value;

commit e00531abf0639df474cba8ed30cd1156aa77cacc

log: fixed a bug with error message introduced by commit cbd2bfec7e

log/log.h
@@ -152,6 +152,8 @@ public:
     _message.append(o); return *this; }
   inline LogHelper &operator<<(const QChar &o) {
     _message.append(o); return *this; }
+  inline LogHelper &operator<<(const char *o) {
+    _message.append(o); return *this; }
   inline LogHelper &operator<<(char o) {
     _message.append(o); return *this; }
   inline LogHelper &operator<<(qint64 o) {