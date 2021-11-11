b7849a2d4bfc269a0169af15bd9bf2c3d3a61bc4
Grégoire Barbier <devel@g76r.eu>
2021-11-11 10:55:33 +0100

    Qt6 support: a #include was lacking

--- util/ioutils.h
+++ util/ioutils.h
@@ -1,4 +1,4 @@
-/* Copyright 2012-2018 Hallowyn, Gregoire Barbier and others.
+/* Copyright 2012-2021 Hallowyn, Gregoire Barbier and others.
  * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
  * Libpumpkin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -17,6 +17,7 @@
 #include <QUrl>
 #include <QStringList>
 #include "libp6core_global.h"
+#include <climits>
 
 class QIODevice;
 
