88f23405743cd87a9a908983f7c108519c012aee
Grégoire Barbier <devel@g76r.eu>
2022-11-11 11:00:01 +0100

    sql: SqlUtils::configureSqlDatabasesFromChildren learnt %-evaluation

--- sql/sqlutils.cpp
+++ sql/sqlutils.cpp
@@ -1,3 +1,16 @@
+/* Copyright 2022 Hallowyn, Gregoire Barbier and others.
+ * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
+ * Libpumpkin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Libpumpkin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with libpumpkin.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "sqlutils.h"
 #include "pf/pfnode.h"
 #include <QSqlDatabase>
@@ -14,9 +27,11 @@ static QRegularExpression _sqldbspec
 
 
 void SqlUtils::configureSqlDatabasesFromChildren(
-  PfNode config, QString childname) {
+  PfNode config, QString childname, ParamsProvider *context) {
   for (auto sqldb: config.childrenByName(childname)) {
     auto spec = sqldb.contentAsString();
+    if (context)
+      spec = context->evaluate(spec);
     auto m = _sqldbspec.match(spec);
     if (!m.hasMatch()) {
       Log::warning() << "cannot parse SQL database specification: " << spec;
--- sql/sqlutils.h
+++ sql/sqlutils.h
@@ -1,9 +1,23 @@
+/* Copyright 2022 Hallowyn, Gregoire Barbier and others.
+ * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
+ * Libpumpkin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Libpumpkin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with libpumpkin.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef SQLUTILS_H
 #define SQLUTILS_H
 
 #include "libp6core_global.h"
 
 class PfNode;
+class ParamsProvider;
 class ParamSet;
 
 class LIBP6CORESHARED_EXPORT SqlUtils {
@@ -24,9 +38,11 @@ public:
    *  password must not contain / or @ (deal with it)
    *  hostname must not contain /
    *  dbname and options must not contain ?
+   *
+   *  @param context if not null, connection string is %-evaluated within it
    */
   static void configureSqlDatabasesFromChildren(
-    PfNode config, QString childname);
+    PfNode config, QString childname, ParamsProvider *context = 0);
   /** calls ParamSet::setValuesFromSqlDb for each child with a given name,
    *  using its children "db", "sql" and "bindings" as parameters.
    *  e.g.
