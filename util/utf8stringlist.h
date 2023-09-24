313b80ad643ab6aec28c8baeedbc35874352cc34
Grégoire Barbier <devel@g76r.eu>
2023-09-24 22:08:01 +0200

    util: introducing Utf8StringIndexedConstList

--- util/utf8stringlist.h
+++ util/utf8stringlist.h
@@ -70,5 +70,95 @@ QDebug LIBP6CORESHARED_EXPORT operator<<(QDebug dbg, const Utf8StringList &l);
 LogHelper LIBP6CORESHARED_EXPORT operator<<(LogHelper lh,
                                              const Utf8StringList &list);
 
+/** Const Utf8String list with reverse-mapping index.
+ *  e.g. Utf8StringIndexedConstList({ "id", "parent", "name", "color"})
+ *          .toIndex().value("name") -> 2 */
+class LIBP6CORESHARED_EXPORT Utf8StringIndexedConstList
+    : public Utf8StringList {
+public:
+  Utf8StringIndexedConstList() { }
+  Utf8StringIndexedConstList(std::initializer_list<Utf8String> args)
+    : Utf8StringList(args) { build_index(); }
+  Utf8StringIndexedConstList(const QList<Utf8String> &list)
+    : Utf8StringList(list) { build_index(); }
+  Utf8StringIndexedConstList(const QList<QByteArray> &list)
+    : Utf8StringList(list.cbegin(), list.cend()) { build_index(); }
+  Utf8StringIndexedConstList(const QList<QString> &list)
+    : Utf8StringList(list.cbegin(), list.cend()) { build_index(); }
+  Utf8StringIndexedConstList(const QSet<Utf8String> &set)
+    : Utf8StringList(set.cbegin(), set.cend()) { build_index(); }
+  Utf8StringIndexedConstList(const QSet<QByteArray> &set)
+    : Utf8StringList(set.cbegin(), set.cend()) { build_index(); }
+  Utf8StringIndexedConstList(const QSet<QString> &set)
+    : Utf8StringList(set.cbegin(), set.cend()) { build_index(); }
+#if __cpp_concepts >= 201907
+  template <std::input_iterator InputIterator>
+  Utf8StringIndexedConstList(InputIterator i1, InputIterator i2)
+    : Utf8StringList(i1, i2) { build_index(); }
+#endif
+  QMap<Utf8String,int> toIndex() const { return _index; }
+
+  // make all non-const methods unavaillable (since index would be inconsistent)
+  void append() = delete;
+  QList::const_reference back() const { return Utf8StringList::back(); }
+  QList::const_iterator begin() const { return Utf8StringList::begin(); }
+  void clear() = delete;
+  QList::const_pointer data() const { return Utf8StringList::data(); }
+  void emplace() = delete;
+  void emplaceBack() = delete;
+  void emplace_back() = delete;
+  QList::const_iterator end() const { return Utf8StringList::end(); }
+  void erase() = delete;
+  void fill() = delete;
+  const Utf8String &first() const { return Utf8StringList::first(); }
+  QList::const_reference front() const { return Utf8StringList::front(); }
+  void insert() = delete;
+  const Utf8String &last() const { return Utf8StringList::last(); }
+  void move() = delete;
+  void pop_back() = delete;
+  void pop_front() = delete;
+  void prepend() = delete;
+  void push_back() = delete;
+  void push_front() = delete;
+  QList::const_reverse_iterator rbegin() const {
+    return Utf8StringList::rbegin(); }
+  void remove() = delete;
+  void removeAll() = delete;
+  void removeAt() = delete;
+  void removeFirst() = delete;
+  void removeIf() = delete;
+  void removeLast() = delete;
+  void removeOne() = delete;
+  QList::const_reverse_iterator rend() const { return Utf8StringList::rend(); }
+  void replace() = delete;
+  void reserve() = delete;
+  void resize() = delete;
+  void shrink_to_fit() = delete;
+  void squeeze() = delete;
+  void swapItemsAt() = delete;
+  void takeAt() = delete;
+  void takeFirst() = delete;
+  void takeLast() = delete;
+  void operator+(int) = delete;
+  void operator+=(int) = delete;
+  void operator|(int) = delete;
+  void operator|=(int) = delete;
+  void operator<<(int) = delete;
+  QList::const_reference operator[](qsizetype i) const {
+    return Utf8StringList::operator[](i); }
+
+private:
+  QMap<Utf8String,int> _index;
+  Utf8StringIndexedConstList &build_index() {
+    QMap<Utf8String,int> index;
+    qsizetype n = size();
+    for (qsizetype i = 0; i < n; ++i)
+      index.insert(operator[](i), i);
+    _index = index;
+    return *this;
+  }
+};
+
+Q_DECLARE_METATYPE(Utf8StringIndexedConstList)
 
 #endif // UTF8STRINGLIST_H
