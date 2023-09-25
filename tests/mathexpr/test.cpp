178dbc16aa57fcda0fa373c7397148e312c1df23
Grégoire Barbier <devel@g76r.eu>
2023-09-25 21:09:57 +0200

    tests: fixing (and enhancing) mathexpr test

--- tests/mathexpr/test.cpp
+++ tests/mathexpr/test.cpp
@@ -5,46 +5,58 @@
 int main(void) {
   QVariant x(ULLONG_MAX/2);
   QVariant y(-132);
+  ParamSet x1 { "x", "1" };
+  ParamSet x1_5 { "x", "1.5" };
+  ParamSet x4 { "x", "4" };
+  ParamSet x5 { "x", "5" };
   qDebug() << x.metaType().name() << y.metaType().name()
            << MathUtils::promoteToBestNumericType(&x, &y)
            << x.metaType().name() << y.metaType().name()
            << x.toLongLong() << y.toLongLong()
            << x.toDouble() << y.toDouble()
-           << (MathUtils::compareQVariantAsNumber(x, y) == QPartialOrdering::Greater)
-           << (MathUtils::compareQVariantAsNumber(x, y) == QPartialOrdering::Unordered)
-           << ParamSet().evaluate("3: %{=rpn,'1,'2,+}")
-           << ParamSet({"x","5"}).evaluate("6: %{=rpn,'1,x,+}")
-           << ParamSet({"x","5"}).evaluate("15: %{=rpn,'1,x,..}")
-           << ParamSet().evaluate(": %{=rpn,'1,',+}")
-           << ParamSet().evaluate("2: %{=rpn,'1,'true,+}")
-           << ParamSet().evaluate("true: %{=rpn,'1,'true,&&}")
-           << ParamSet().evaluate("true: %{=rpn,'1,'true,==}")
-           << ParamSet().evaluate("false: %{=rpn,'42,'true,==}")
-           << ParamSet().evaluate("true: %{=rpn,'42,!!,'true,==}")
-           << ParamSet({"x","1"}).evaluate("33: %{=rpn,'0x20,x,+}")
-           << ParamSet({"x","1.5"}).evaluate("33.5: %{=rpn,'0x20,x,+}")
-           << ParamSet({"x","1.5"}).evaluate("2001.5: %{=rpn,'2k,x,+}")
-           << ParamSet({"x","4"}).evaluate("4: %{=rpn,'1,'2,==,'3,x,?:}")
-           << ParamSet().evaluate("true: %{=rpn,'aabcdaa,'bc,=~}")
-           << ParamSet().evaluate("false: %{=rpn,'aabcdaa,'bC,=~}")
-           << ParamSet().evaluate("false: %{=rpn,'aabcdaa,'c$,=~}")
-           << ParamSet().evaluate("true: %{=rpn,'aabcdaa,'a$,=~}")
+           << (MathUtils::compareQVariantAsNumberOrString(x, y) == QPartialOrdering::Greater)
+           << (MathUtils::compareQVariantAsNumberOrString(x, y) == QPartialOrdering::Unordered)
+           << PercentEvaluator::eval_utf8("3: %{=rpn,'1,'2,+}")
+           << PercentEvaluator::eval("%{=rpn,'1,'2,+}")
+           << PercentEvaluator::eval_utf8("6: %{=rpn,'1,x,+}", &x5)
+           << PercentEvaluator::eval_utf8("15: %{=rpn,'1,x,..}", &x5)
+           << PercentEvaluator::eval_utf8(": %{=rpn,'1,',+}")
+           << PercentEvaluator::eval("%{=rpn,'1,',+}")
+           << PercentEvaluator::eval_utf8("2: %{=rpn,'1,'true,+}")
+           << PercentEvaluator::eval_utf8("true: %{=rpn,'1,'true,&&}")
+           << PercentEvaluator::eval("%{=rpn,'1,'true,&&}")
+           << PercentEvaluator::eval_utf8("true: %{=rpn,'1,'true,==}")
+           << PercentEvaluator::eval_utf8("false: %{=rpn,'42,'true,==}")
+           << PercentEvaluator::eval_utf8("true: %{=rpn,'42,!!,'true,==}")
+           << PercentEvaluator::eval_utf8("33: %{=rpn,'0x20,x,+}", &x1)
+           << PercentEvaluator::eval_utf8("33.5: %{=rpn,'0x20,x,+}", &x1_5)
+           << PercentEvaluator::eval_utf8("2001.5: %{=rpn,'2k,x,+}", &x1_5)
+           << PercentEvaluator::eval("%{=rpn,'2k,x,+}", &x1_5)
+           << PercentEvaluator::eval_utf8("4: %{=rpn,'1,'2,==,'3,x,?:}", &x4)
+           << PercentEvaluator::eval_utf8("true: %{=rpn,'aabcdaa,'bc,=~}")
+           << PercentEvaluator::eval_utf8("false: %{=rpn,'aabcdaa,'bC,=~}")
+           << PercentEvaluator::eval_utf8("false: %{=rpn,'aabcdaa,'c$,=~}")
+           << PercentEvaluator::eval_utf8("true: %{=rpn,'aabcdaa,'a$,=~}")
     ;
   ParamSet p { "foo", "bar", "empty", "", "x", "42" };
-  qDebug() << p.evaluate("%{=rpn,empty,?-}=false %{=rpn,empty,?*}=true "
+  qDebug() << PercentEvaluator::eval_utf8(
+              "%{=rpn,empty,?-}=false %{=rpn,empty,?*}=true "
               "%{=rpn,inexistent,?-}=false %{=rpn,inexistent,?*}=false "
               "%{=rpn,empty,foo,??}=bar %{=rpn,empty,foo,??*}= "
               "%{=rpn,inexistent,foo,??}=bar %{=rpn,inexistent,foo,??*}=bar "
               "%{=rpn,empty,inexistent,==,'ø,??*}=true %{=rpn,empty,inexistent,==*,'ø,??*}=ø "
-              "%{=rpn,empty,inexistent,!=,'ø,??*}=false %{=rpn,empty,inexistent,!=*,'ø,??*}=ø ");
-  qDebug() << p.evaluate("%{=rpn,foo,inexistent,>?}=bar %{=rpn,foo,inexistent,>?*,'ø,??*}=ø "
+              "%{=rpn,empty,inexistent,!=,'ø,??*}=false %{=rpn,empty,inexistent,!=*,'ø,??*}=ø ", &p);
+  qDebug() << PercentEvaluator::eval_utf8("%{=rpn,foo,inexistent,>?}=bar %{=rpn,foo,inexistent,>?*,'ø,??*}=ø "
               "%{=rpn,'0xffffffffffffffff','1,+,'ø,??*}=ø %{=rpn,'1,'foo,+,'ø,??*}=ø "
               "%{=rpn,'0xfffffffffffffffe','1,+,'ø,??*}=18446744073709551615 "
-              "%{=rpn,'abc,'12,'13,==,..}=abcfalse ");
-  qDebug() << p.evaluate("%{=rpn,x,'true,&&,'ø,??*}=true %{=rpn,x,empty,&&,'ø,??*}=ø "
+              "%{=rpn,'abc,'12,'13,==,..}=abcfalse ", &p);
+  qDebug() << PercentEvaluator::eval_utf8("%{=rpn,x,'true,&&,'ø,??*}=true %{=rpn,x,empty,&&,'ø,??*}=ø "
               "%{=rpn,x,nonexistent,&&,'ø,??*}=ø %{=rpn,<pi>}=3.141592653589793 "
-              "%{=rpn,<null>}= %{=rpn,',?*}=true %{=rpn,<nil>,?*}=false");
-  qDebug() << p.evaluate("%{=rpn,foo}=bar %{=rpn,'foo}=foo %{=rpn,'%foo}=bar");
-  qDebug() << p.evaluate("%{=rpn,=rpn;'42;!!,'z,..}=truez %{=rpn,'dt: ,=date,..}");
+              "%{=rpn,<null>}= %{=rpn,',?*}=true %{=rpn,<nil>,?*}=false", &p);
+  qDebug() << PercentEvaluator::eval_utf8("%{=rpn,foo}=bar %{=rpn,'foo}=foo "
+              "%{=rpn,'%foo}=%foo", // =rpn does not %evaluate it terms
+              &p);
+  qDebug() << PercentEvaluator::eval_utf8("%{=rpn,=rpn;'42;!!,'z,..}=truez "
+              "%{=rpn,'dt: ,=date**2023,..}=dt: 2023-09-20 00:00:00,000", &p);
   return 0;
 }
