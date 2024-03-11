9ba32e80a2dd00e6bade6c7beebde06bc1e90353
Grégoire Barbier <devel@g76r.eu>
2024-03-11 06:36:42 +0100

    modelview: enhanced 2 error messages

--- modelview/inmemoryshareduiitemdocumentmanager.cpp
+++ modelview/inmemoryshareduiitemdocumentmanager.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2015-2023 Hallowyn, Gregoire Barbier and others.
+/* Copyright 2015-2024 Hallowyn, Gregoire Barbier and others.
  * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
  * Libpumpkin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -48,6 +48,6 @@ SharedUiItemList InMemorySharedUiItemDocumentManager::itemsByQualifier(
   SharedUiItemList list;
   if (!_repository.contains(qualifier))
     Log::warning() << "itemsByQualifier() called with id qualifier not found "
-                      "in repository:" << qualifier;
+                      "in repository: " << qualifier;
   return _repository.value(qualifier).values();
 }
--- modelview/shareduiitem.cpp
+++ modelview/shareduiitem.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2014-2023 Hallowyn, Gregoire Barbier and others.
+/* Copyright 2014-2024 Hallowyn, Gregoire Barbier and others.
  * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
  * Libpumpkin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -61,7 +61,7 @@ bool SharedUiItemData::setUiData(
   Q_ASSERT(errorString != 0);
   *errorString =
       QObject::tr("Field \"%1\" is not ui-editable for item of type %2")
-      .arg(uiHeaderData(section, Qt::DisplayRole).toString())
+      .arg(uiHeaderData(section, Qt::DisplayRole) | Utf8String::number(section))
       .arg(qualifier());
   return false;
 }
