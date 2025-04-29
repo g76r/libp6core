g76r/libp6core

commit 114c08fa323e78003599df8309a42bbb1265ea7a

format: GraphvizRenderer can now pass custom command line options
such as: -s1.0 -Gsplines=spline -n2

please don't mess with -o and -T ;-)

format/graphvizrenderer.cpp
@@ -65,7 +65,10 @@ void GraphvizRenderer::do_start(
     _timout_timer->setSingleShot(true);
     _timout_timer->start(timeoutms);
   }
-  QProcess::start(layoutAsString(layout), {{"-T"_u8 + formatAsString(format)}});
+  auto command = layout == UnknownLayout ? "false"_u8 : layoutAsString(layout);
+  QStringList options = _options;
+  options << "-T"+formatAsString(format);
+  QProcess::start(command, _options);
   waitForStarted();
   qint64 written = write(source);
   if (written != source.size())

format/graphvizrenderer.h
@@ -39,6 +39,7 @@ private:
   QMutex _mutex;
   int _timeoutms = 0;
   QTimer *_timout_timer = 0;
+  QStringList _options;
 
 public:
   GraphvizRenderer(
@@ -87,6 +88,9 @@ public:
   Utf8String run(ParamsProvider *params_evaluation_context = 0,
                  const Utf8String &source = {});
   inline Utf8String run(const Utf8String &source) { return run(0, source); }
+  inline QStringList options() const { return _options; }
+  /** Set custom command line options, such as "-Gsplines=spline" or "-n2" */
+  inline void set_options(const QStringList &options) { _options = options; }
   static Utf8String mime_type(Format format);
   static Format formatFromString(const Utf8String &s, Format def = Gv);
   static Utf8String formatAsString(Format format);

commit 4c61bde28f5afcd0121fd3722cd0fe375e58f1ae

format: fixed GraphvizRenderer no longer supporting other formats than gv
i.e. no longer supporting svg, png...
regression was introduced in commit 114c08fa323e78003 on 2025-04-10

format/graphvizrenderer.cpp
@@ -19,6 +19,7 @@
 #include <QThread>
 #include <QTimer>
 #include <QtDebug>
+#include <QDateTime>
 
 GraphvizRenderer::GraphvizRenderer(QObject *parent,
     const Utf8String &source, Layout layout, Format format, int timeoutms,
@@ -68,7 +69,7 @@ void GraphvizRenderer::do_start(
   options << "-T"+formatAsString(format);
   qDebug() << "graphviz rendering process starting" << command << options;
   _startms = QDateTime::currentMSecsSinceEpoch();
-  QProcess::start(command, _options);
+  QProcess::start(command, options);
   waitForStarted();
   qint64 written = write(source);
   if (written != source.size())