g76r/libp6core

commit bfcd631e9b3317ae015ba82eac365c69c9b5731d

log: large re-engineering of log framework, api, thread bug fixes...
* p6::log:: namespace instead of ::Log static class
* rewritten thread/model/logger fixing some shutdown bugs
* addConsoleLogger uses stderr by default (was: hard coded stdout)
  making it consistent with QtDebug and Unix conventions
  (still need setbuf(stderr, 0); in main on windows since line buffering
  doesn't exist (= full buffering is used by default instead, messing up
  the logs)
* providing api for custom string location
* more explicit class and methods names, more snake_case, more utf8

yet to come: getting rid of the mutex

log/filelogger.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2012-2023 Hallowyn, Gregoire Barbier and others.
+/* Copyright 2012-2025 Hallowyn, Gregoire Barbier and others.
  * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
  * Libpumpkin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -17,12 +17,13 @@
 
 #define ISO8601 u"yyyy-MM-ddThh:mm:ss,zzz"_s
 
-FileLogger::FileLogger(QIODevice *device, Log::Severity minSeverity,
+namespace p6::log {
+
+FileLogger::FileLogger(QIODevice *device, Severity minSeverity,
                        bool buffered)
-  : Logger(minSeverity, Logger::DedicatedThread), _device(0),
+  : Logger(minSeverity, Logger::DedicatedThread), _device(device),
     _buffered(buffered) {
   //qDebug() << "creating FileLogger from device" << device;
-  _device = device;
   /*qDebug() << "FileLogger::FileLoger" << this->thread() << _device->thread()
            << QThread::currentThread();
   qDebug() << "/FileLogger::setParent";*/
@@ -32,13 +33,13 @@ FileLogger::FileLogger(QIODevice *device, Log::Severity minSeverity,
                        |QIODevice::Unbuffered)) [[unlikely]] {
       qWarning() << "cannot open log device" << _device << ":"
                  << _device->errorString();
-      delete _device;
+      _device->deleteLater();
       _device = 0;
     }
   }
 }
 
-FileLogger::FileLogger(QString pathPattern, Log::Severity minSeverity,
+FileLogger::FileLogger(QString pathPattern, Severity minSeverity,
                        int secondsReopenInterval, bool buffered)
   : Logger(minSeverity, Logger::DedicatedThread), _device(0),
     _pathPattern(pathPattern), _lastOpen(QDateTime::currentDateTime()),
@@ -48,18 +49,18 @@ FileLogger::FileLogger(QString pathPattern, Log::Severity minSeverity,
 
 FileLogger::~FileLogger() {
   if (_device)
-    delete _device;
+    _device->deleteLater();
 }
 
-Utf8String FileLogger::currentPath() const {
+Utf8String FileLogger::current_path() const {
   return _currentPath;
 }
 
-Utf8String FileLogger::pathPattern() const {
+Utf8String FileLogger::path_pattern() const {
   return _pathPattern;
 }
 
-void FileLogger::doLog(const LogEntry &entry) {
+void FileLogger::do_log(const Record &record) {
   QDateTime now = QDateTime::currentDateTime();
   if (!_pathPattern.isEmpty()
       && (_device == 0
@@ -68,7 +69,7 @@ void FileLogger::doLog(const LogEntry &entry) {
     //qDebug() << "*******************************************************"
     //         << _pathPattern << _lastOpen << now << _secondsReopenInterval;
     if (_device)
-      delete _device;
+      _device->deleteLater();
     _currentPath = Utf8String(PercentEvaluator::eval(_pathPattern));
     _device = new QFile(_currentPath);
     if (!_device->open(_buffered ? QIODevice::WriteOnly|QIODevice::Append
@@ -77,7 +78,7 @@ void FileLogger::doLog(const LogEntry &entry) {
       // TODO warn, but only once
       //qWarning() << "cannot open log file" << _currentPath << ":"
       //           << _device->errorString();
-      delete _device;
+      _device->deleteLater();
       _device = 0;
     } else {
       _lastOpen = QDateTime::currentDateTime();
@@ -85,14 +86,7 @@ void FileLogger::doLog(const LogEntry &entry) {
     }
   }
   if (_device) [[likely]] {
-    // TODO move this to LogEntry::asLogLine()
-    Utf8String line =
-        Utf8String(entry.timestamp().toString(ISO8601))+" "_u8
-        +entry.taskid()+"/"_u8+entry.execid()+" "_u8+entry.location()+" "_u8
-        +entry.severityToString()+" "_u8+entry.message()+"\n"_u8;
-    //qDebug() << "***log" << line;
-    //if (_pathPattern.endsWith(".slow") && (QTime::currentTime().second()/10)%2)
-    //  ::usleep(1000000);
+    auto line = record.formated_message();
     if (_device->write(line) != line.size()) {
       // TODO warn, but only once
       //qWarning() << "error while writing log:" << _device
@@ -106,10 +100,13 @@ void FileLogger::doLog(const LogEntry &entry) {
   }
 }
 
-void FileLogger::doShutdown() {
+void FileLogger::do_shutdown() {
+  // TODO only if buffered ?
   _buffered = false;
   if (_device && !_pathPattern.isEmpty()) {
     _device->close();
     _device->open(QIODevice::WriteOnly|QIODevice::Append|QIODevice::Unbuffered);
   }
 }
+
+} // ns p6::log

log/filelogger.h
@@ -1,4 +1,4 @@
-/* Copyright 2012-2023 Hallowyn, Gregoire Barbier and others.
+/* Copyright 2012-2025 Hallowyn, Gregoire Barbier and others.
  * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
  * Libpumpkin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -19,6 +19,8 @@
 class QIODevice;
 class QThread;
 
+namespace p6::log {
+
 class LIBP6CORESHARED_EXPORT FileLogger : public Logger {
   Q_OBJECT
   Q_DISABLE_COPY(FileLogger)
@@ -31,19 +33,21 @@ class LIBP6CORESHARED_EXPORT FileLogger : public Logger {
 public:
   /** Takes ownership of the device (= will delete it).
     * @param buffered only applies if device is not already open */
-  explicit FileLogger(QIODevice *device, Log::Severity minSeverity = Log::Info,
+  explicit FileLogger(QIODevice *device, Severity minSeverity = Info,
                       bool buffered = true);
   explicit FileLogger(QString pathPattern,
-                      Log::Severity minSeverity = Log::Info,
+                      Severity minSeverity = Info,
                       int secondsReopenInterval = 300,
                       bool buffered = true);
   ~FileLogger();
-  Utf8String currentPath() const override;
-  Utf8String pathPattern() const override;
+  Utf8String current_path() const override;
+  Utf8String path_pattern() const override;
 
 protected:
-  void doLog(const LogEntry &entry) override;
-  void doShutdown() override;
+  void do_log(const Record &record) override;
+  void do_shutdown() override;
 };
+
+} // ns p6::log
 
 #endif // FILELOGGER_H

log/log.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2012-2023 Hallowyn, Gregoire Barbier and others.
+/* Copyright 2012-2025 Hallowyn, Gregoire Barbier and others.
  * This file is part of libpumpkin, see <http://libpumpkin.g76r.eu/>.
  * Libpumpkin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -15,7 +15,7 @@
 #include "multiplexerlogger.h"
 #include <QThread>
 
-#define ISO8601 u"yyyy-MM-ddThh:mm:ss,zzz"_s
+namespace p6::log {
 
 static MultiplexerLogger *_rootLogger = nullptr;
 static QMutex *_qtHandlerMutex = nullptr;
@@ -23,90 +23,109 @@ static QtMessageHandler _qtOriginalHandler = nullptr;
 
 /******************************************************************
   /!\ there must be no global variables with a destructor here /!\
-  /!\ because Log::log() must not crash when called during the /!\
-  /!\ program shutdown                                         /!\
+  /!\ because p6::log::log() must not crash when called during /!\
+  /!\ the program shutdown                                     /!\
  ******************************************************************/
 
-static inline void sanitizeField(QByteArray *ba) {
-  if (ba->isNull()) [[unlikely]]
-    return;
-  for (char *s = ba->data(); *s; ++s)
-    if (::isspace(*s)) [[unlikely]]
-      *s = '_';
-}
-
-static inline void sanitizeMessage(QByteArray *ba) {
-  ba->replace("\n","\n ");
-}
-
-void Log::addLogger(Logger *logger, bool autoRemovable) {
+void addLogger(Logger *logger, bool autoRemovable) {
   if (!_rootLogger)
     return;
-  if (logger->threadModel() == Logger::DirectCall)
+  if (logger->thread_model() == Logger::DirectCall)
     logger->moveToThread(_rootLogger->thread());
   _rootLogger->addLogger(logger, autoRemovable);
 }
 
-void Log::removeLogger(Logger *logger) {
+void removeLogger(Logger *logger) {
   if (!_rootLogger)
     return;
   _rootLogger->removeLogger(logger);
 }
 
-void Log::addConsoleLogger(Severity severity, bool autoRemovable) {
+void addConsoleLogger(
+    Severity severity, bool autoRemovable, FILE *stream) {
   if (!_rootLogger)
     return;
-  _rootLogger->addConsoleLogger(severity, autoRemovable);
+  _rootLogger->addConsoleLogger(severity, autoRemovable, stream);
 }
 
-void Log::replaceLoggers(Logger *newLogger) {
+void replaceLoggers(Logger *newLogger) {
   if (!_rootLogger)
     return;
   _rootLogger->replaceLoggers(newLogger);
 }
 
-void Log::replaceLoggers(QList<Logger*> newLoggers) {
+void replaceLoggers(QList<Logger*> newLoggers) {
   if (!_rootLogger)
     return;
   _rootLogger->replaceLoggers(newLoggers);
 }
 
-void Log::replaceLoggersPlusConsole(Log::Severity consoleLoggerSeverity,
-                                    QList<Logger*> newLoggers) {
+void replaceLoggersPlusConsole(
+    Severity consoleLoggerSeverity, QList<Logger*> newLoggers) {
   if (!_rootLogger)
     return;
   _rootLogger->replaceLoggersPlusConsole(
         consoleLoggerSeverity, newLoggers);
 }
 
-Utf8String LogContext::current_thread_name() {
-  QThread *t = QThread::currentThread();
-  return t ? Utf8String{t->objectName()} : Utf8String{};
+Utf8String Record::current_thread_name() {
+  if (QThread *t = QThread::currentThread(); t)
+    return t->objectName();
+  return {};
 }
 
-void Log::log(Utf8String message, Severity severity, LogContext context) {
+qint64 Record::now() {
+  return QDateTime::currentMSecsSinceEpoch();
+}
+
+inline static Utf8String sanitized_message(const Utf8String &input) {
+  const char *begin = input.constData(), *s = begin;
+  for (; *s; ++s)
+    if (*s == '\n') [[unlikely]]
+      goto eol_found;
+  return input; // short path: copy nothing because there was no \n
+eol_found:;
+  Utf8String output = input.sliced(0, s-begin);
+  for (; *s; ++s)
+    if (*s == '\n' && s[1])
+      output += "\n "_u8;
+    else
+      output += *s;
+  return output;
+}
+
+Utf8String Record::formated_message() const {
+  const static QString _timestamp_format = "yyyy-MM-ddThh:mm:ss,zzz ";
+  Utf8String ts{QDateTime::fromMSecsSinceEpoch(_timestamp)
+        .toString(_timestamp_format)};
+  if (ts.isEmpty()) [[unlikely]] // on late shutdown QDateTime ceases to work
+    ts = Utf8String::number(_timestamp/1e3, 'f', 3)+" "_u8;
+  return ts+_taskid+"/"_u8+_execid+" "_u8+_location+" "_u8
+      +severity_as_text(_severity)+" "_u8+sanitized_message(_message)+"\n"_u8;
+}
+
+void log(const Record &record) {
   if (!_rootLogger)
     return;
-  QDateTime now = QDateTime::currentDateTime();
-  sanitizeMessage(&message);
-  _rootLogger->log(Logger::LogEntry(now, message, severity, context));
+  _rootLogger->log(record);
 }
 
-void Log::init() {
+void init() {
   if (_rootLogger)
     return;
-  _rootLogger = new MultiplexerLogger(Log::Debug, true);
+  _rootLogger = new MultiplexerLogger(Debug, true);
   _qtHandlerMutex = new QMutex;
 }
 
-void Log::shutdown() {
+void shutdown() {
   if (!_rootLogger)
     return;
-  _rootLogger->shutdown();
+  auto rl = _rootLogger;
   _rootLogger = nullptr;
+  rl->shutdown();
 }
 
-Utf8String Log::severityToString(Severity severity) {
+Utf8String severity_as_text(Severity severity) {
   switch (severity) {
   case Debug:
     return "DEBUG"_u8;
@@ -122,81 +141,53 @@ Utf8String Log::severityToString(Severity severity) {
   return "UNKNOWN"_u8;
 }
 
-Log::Severity Log::severityFromString(Utf8String string) {
-  switch (string.value(0)) {
-    case 'I':
-    case 'i':
+inline static Severity severity_from_qttype(QtMsgType type) {
+  switch (type) {
+    case QtInfoMsg:
       return Info;
-    case 'W':
-    case 'w':
+    case QtWarningMsg:
       return Warning;
-    case 'E':
-    case 'e':
+    case QtCriticalMsg:
       return Error;
-    case 'F':
-    case 'f':
+    case QtFatalMsg:
       return Fatal;
+    case QtDebugMsg:
+      ;
   }
   return Debug;
 }
 
-QString Log::pathToLastFullestLog() {
+QString pathToLastFullestLog() {
   if (!_rootLogger)
     return {};
   return _rootLogger->pathToLastFullestLog();
 }
 
-QStringList Log::pathsToFullestLogs() {
+QStringList pathsToFullestLogs() {
   if (!_rootLogger)
     return {};
   return _rootLogger->pathsToFullestLogs();
 }
 
-QStringList Log::pathsToAllLogs() {
+QStringList pathsToAllLogs() {
   if (!_rootLogger)
     return {};
   return _rootLogger->pathsToAllLogs();
 }
 
 static void qtLogSamePatternWrapper(
-    QtMsgType type, const QMessageLogContext &context, const QString &msg) {
-  Utf8String severity = "UNKNOWN"_u8;
-  switch (type) {
-    case QtDebugMsg:
-      severity = Log::severityToString(Log::Debug);
-      break;
-    case QtInfoMsg:
-      severity = Log::severityToString(Log::Info);
-      break;
-    case QtWarningMsg:
-      severity = Log::severityToString(Log::Warning);
-      break;
-    case QtCriticalMsg:
-      severity = Log::severityToString(Log::Error);
-      break;
-    case QtFatalMsg:
-      severity = Log::severityToString(Log::Fatal);
-      break;
-  }
-  Utf8String taskid = QThread::currentThread()->objectName();
-  sanitizeField(&taskid);
-  if (taskid.isEmpty())
-    taskid = "?"_u8;
-  Utf8String realMsg = msg;
-  sanitizeMessage(&realMsg);
-  Utf8String location =
-      context.file
-      ? context.file+":"_u8+Utf8String::number(context.line) : ":"_u8;
-  Utf8String localMsg =
-    Utf8String(QDateTime::currentDateTime().toString(ISO8601))
-      +" "_u8+taskid+"/0 "_u8+location+" "_u8+severity+" qtdebug: "_u8+realMsg
-      +"\n"_u8;
-  fputs(localMsg, stderr);
-  if (type == QtFatalMsg)
+    QtMsgType type, const QMessageLogContext &qtcontext, const QString &msg) {
+  auto severity = severity_from_qttype(type);
+  Utf8String location;
+  if (qtcontext.file) [[likely]]
+    location = qtcontext.file+":"_u8+Utf8String::number(qtcontext.line);
+  fputs(Record(severity, {}, {}, location).set_message(msg)
+        .formated_message(), stderr);
+  if (type == QtFatalMsg) [[unlikely]]
     abort();
 }
 
-void Log::wrapQtLogToSamePattern(bool enable) {
+void wrapQtLogToSamePattern(bool enable) {
   if (!_rootLogger)
     return;
   QMutexLocker ml(_qtHandlerMutex);
@@ -209,3 +200,5 @@ void Log::wrapQtLogToSamePattern(bool enable) {
     _qtOriginalHandler = 0;
   }
 }
+
+} // ns p6::log

log/multiplexerlogger.cpp
@@ -17,6 +17,8 @@
 #include <QFile>
 #include <QThread>
 
+namespace p6::log {
+
 MultiplexerLogger::MultiplexerLogger(
     Log::Severity minSeverity, bool isRootLogger)
   : Logger(minSeverity, isRootLogger ? Logger::RootLogger
@@ -35,13 +37,13 @@ void MultiplexerLogger::addLogger(Logger *logger, bool autoRemovable) {
     // LATER provide an option to enable Qt's standard log interception
     // drawbacks:
     // - Qt's log is synchronous (no writer thread) and thus intercepting it
-    //   would change the behavior (log order, even missing log entries on
+    //   would change the behavior (log order, even missing log records on
     //   crash)
     // - qFatal() expect the program to write a log and shutdown, which is not
     //   easy to reproduce here
     //if (_loggers.isEmpty())
     //  qInstallMsgHandler(Log::logMessageHandler);
-    logger->_autoRemovable = autoRemovable;
+    logger->_auto_removable = autoRemovable;
     _loggers.append(logger);
   }
 }
@@ -59,14 +61,14 @@ void MultiplexerLogger::removeLogger(Logger *logger) {
 }
 
 void MultiplexerLogger::addConsoleLogger(
-  Log::Severity severity, bool autoRemovable) {
+  Log::Severity severity, bool autoRemovable, FILE *stream) {
   QFile *console = new QFile;
-  console->open(1, QIODevice::WriteOnly|QIODevice::Unbuffered);
-  FileLogger *logger = new FileLogger(console, severity);
+  console->open(stream, QIODevice::WriteOnly|QIODevice::Unbuffered);
+  FileLogger *logger = new FileLogger(console, severity, false);
   auto name = "Console"+logger->objectName();
   logger->setObjectName(name);
-  if (logger->thread())
-    logger->thread()->setObjectName(name);
+  if (auto t = logger->thread(); t) // should always be true
+    t->setObjectName(name);
   addLogger(logger, autoRemovable);
 }
 
@@ -96,7 +98,7 @@ void MultiplexerLogger::doReplaceLoggers(QList<Logger*> newLoggers) {
   QList<Logger*> old_loggers(_loggers);
   old_loggers.detach();
   for (auto logger: old_loggers)
-    if (logger->_autoRemovable) {
+    if (logger->_auto_removable) {
       if (!newLoggers.contains(logger))
         logger->shutdown();
       _loggers.removeAll(logger);
@@ -109,16 +111,16 @@ void MultiplexerLogger::doReplaceLoggers(QList<Logger*> newLoggers) {
 QString MultiplexerLogger::pathToLastFullestLog() {
   // LATER avoid locking here whereas right logger won't change often
   QMutexLocker locker(&_loggersMutex);
-  int severity = Log::Fatal+1;
+  int severity = Fatal+1;
   QString path;
   for (auto logger: _loggers) {
-    if (logger->minSeverity() < severity) {
-      QString p = logger->currentPath();
+    if (logger->min_severity() < severity) {
+      QString p = logger->current_path();
       if (!p.isEmpty()) {
-        if (severity == Log::Debug)
+        if (severity == Debug)
           return p;
         path = p;
-        severity = logger->minSeverity();
+        severity = logger->min_severity();
       }
     }
   }
@@ -128,18 +130,18 @@ QString MultiplexerLogger::pathToLastFullestLog() {
 QStringList MultiplexerLogger::pathsToFullestLogs() {
   // LATER avoid locking here whereas right logger won't change often
   QMutexLocker locker(&_loggersMutex);
-  int severity = Log::Fatal+1;
+  int severity = Fatal+1;
   QString path;
   for (auto logger: _loggers) {
-    if (logger->minSeverity() < severity) {
-      QString p = logger->pathMatchingRegexp();
+    if (logger->min_severity() < severity) {
+      QString p = logger->path_matching_regexp();
       if (!p.isEmpty()) {
-        if (severity == Log::Debug) {
+        if (severity == Debug) {
           locker.unlock();
           return IOUtils::findFiles(p);
         }
         path = p;
-        severity = logger->minSeverity();
+        severity = logger->min_severity();
       }
     }
   }
@@ -152,7 +154,7 @@ QStringList MultiplexerLogger::pathsToAllLogs() {
   QMutexLocker locker(&_loggersMutex);
   QStringList paths;
   for (auto logger: _loggers) {
-    QString p = logger->pathMatchingRegexp();
+    QString p = logger->path_matching_regexp();
     if (!p.isEmpty())
       paths.append(p);
   }
@@ -160,30 +162,31 @@ QStringList MultiplexerLogger::pathsToAllLogs() {
   return IOUtils::findFiles(paths);
 }
 
-void MultiplexerLogger::doLog(const LogEntry &entry) {
+void MultiplexerLogger::do_log(const Record &record) {
   QMutexLocker locker(&_loggersMutex);
   for (auto logger : _loggers)
-    logger->log(entry);
-  if (_threadModel == RootLogger && _loggers.isEmpty() && !!entry) {
-    switch(entry.severity()) {
-      using enum Log::Severity;
+    logger->log(record);
+  if (_thread_model & RootLogger && _loggers.isEmpty() && !!record) {
+    switch(record.severity()) {
       case Debug:
-        qDebug() << entry.message() << "(no logger configured)";
+        qDebug() << record.formated_message() << "(no logger configured)";
         break;
       case Info:
-        qInfo() << entry.message() << "(no logger configured)";
+        qInfo() << record.formated_message() << "(no logger configured)";
         break;
       case Warning:
       case Error:
       case Fatal:
-        qWarning() << entry.message() << "(no logger configured)";
+        qWarning() << record.formated_message() << "(no logger configured)";
     }
   }
 }
 
-void MultiplexerLogger::doShutdown() {
+void MultiplexerLogger::do_shutdown() {
   QMutexLocker locker(&_loggersMutex);
   for (auto logger : _loggers)
     logger->shutdown();
   _loggers.clear();
 }
+
+} // ns p6::log

commit 2519e0b3e6a47ae22ac2e3f874b1b21b5e1cb377

log: fixed a filedescriptor and memory leak
probably introduced by commit bfcd631e9b3317ae

QFile objects were not deleted, which is a rather fast leak since they
are recreated every n seconds (for instance: 60) when logging to a file
with a name pattern

log/filelogger.cpp
@@ -15,6 +15,7 @@
 #include "util/paramset.h"
 #include <QFile>
 #include <QDateTime>
+#include <QCoreApplication>
 
 #define ISO8601 u"yyyy-MM-ddThh:mm:ss,zzz"_s
 
@@ -75,6 +76,8 @@ void FileLogger::do_log(const Record &record) {
       _device->deleteLater();
     _currentPath = Utf8String(PercentEvaluator::eval(_pathPattern));
     _device = new QFile(_currentPath);
+    _device->setObjectName(_device->objectName()+" from "+this->objectName());
+    _device->moveToThread(QCoreApplication::instance()->thread());
     if (!_device->open(_buffered ? QIODevice::WriteOnly|QIODevice::Append
                                  : QIODevice::WriteOnly|QIODevice::Append
                                      |QIODevice::Unbuffered)) [[unlikely]] {

log/filelogger.h
@@ -15,6 +15,7 @@
 #define FILELOGGER_H
 
 #include "logger.h"
+#include <QDateTime>
 
 class QIODevice;
 class QThread;

log/log.cpp
@@ -49,24 +49,13 @@ void addConsoleLogger(
   _rootLogger->addConsoleLogger(severity, autoRemovable, stream);
 }
 
-void replaceLoggers(Logger *newLogger) {
+void replace_loggers(
+    QList<Logger*> &new_loggers, bool prepend_console,
+    Severity console_min_severity) {
   if (!_rootLogger)
     return;
-  _rootLogger->replaceLoggers(newLogger);
-}
-
-void replaceLoggers(QList<Logger*> newLoggers) {
-  if (!_rootLogger)
-    return;
-  _rootLogger->replaceLoggers(newLoggers);
-}
-
-void replaceLoggersPlusConsole(
-    Severity consoleLoggerSeverity, QList<Logger*> newLoggers) {
-  if (!_rootLogger)
-    return;
-  _rootLogger->replaceLoggersPlusConsole(
-        consoleLoggerSeverity, newLoggers);
+  _rootLogger->replace_loggers(new_loggers, prepend_console,
+                               console_min_severity);
 }
 
 Utf8String Record::current_thread_name() {

log/log.h
@@ -192,14 +192,29 @@ void LIBP6CORESHARED_EXPORT wrapQtLogToSamePattern(bool enable = true);
 void LIBP6CORESHARED_EXPORT addConsoleLogger(
     Severity severity = Warning, bool autoRemovable = false,
     FILE *stream = stderr);
-/** Remove loggers that are autoremovable and replace them with a new one. */
-void LIBP6CORESHARED_EXPORT replaceLoggers(Logger *newLogger);
-/** Remove loggers that are autoremovable and replace them with new ones. */
-void LIBP6CORESHARED_EXPORT replaceLoggers(QList<Logger*> newLoggers);
-/** Remove loggers that are autoremovable and replace them with new ones
-   * plus a console logger.*/
-void LIBP6CORESHARED_EXPORT replaceLoggersPlusConsole(
-    Severity consoleLoggerSeverity, QList<Logger*> newLoggers);
+/** Remove loggers that are autoremovable and replace them with new ones.
+ *  Optionaly prepend a console logger. */
+void LIBP6CORESHARED_EXPORT replace_loggers(
+    QList<Logger*> &new_loggers, bool prepend_console = false,
+    Severity console_min_severity = Fatal);
+
+[[deprecated("use replace_loggers instead")]]
+inline void replaceLoggers(Logger *newLogger) {
+  QList<Logger*> list{newLogger};
+  replace_loggers(list);
+}
+
+[[deprecated("use replace_loggers instead")]]
+inline void replaceLoggers(QList<Logger*> &newLoggers) {
+  replace_loggers(newLoggers);
+}
+
+[[deprecated("use replace_loggers instead")]]
+inline void replaceLoggersPlusConsole(
+    Severity consoleLoggerSeverity, QList<Logger *> &newLoggers) {
+  return replace_loggers(newLoggers, true, consoleLoggerSeverity);
+}
+
 /** init log engine */
 void LIBP6CORESHARED_EXPORT init();
 /** flush remove any logger */

log/multiplexerlogger.cpp
@@ -65,6 +65,7 @@ void MultiplexerLogger::addConsoleLogger(
   QFile *console = new QFile;
   console->open(stream, QIODevice::WriteOnly|QIODevice::Unbuffered);
   FileLogger *logger = new FileLogger(console, severity, false);
+  console->setParent(logger);
   auto name = "Console"+logger->objectName();
   logger->setObjectName(name);
   if (auto t = logger->thread(); t) // should always be true
@@ -72,38 +73,28 @@ void MultiplexerLogger::addConsoleLogger(
   addLogger(logger, autoRemovable);
 }
 
-void MultiplexerLogger::replaceLoggers(Logger *newLogger) {
-  QList<Logger*> newLoggers;
-  if (newLogger)
-    newLoggers.append(newLogger);
-  replaceLoggers(newLoggers);
-}
-
-void MultiplexerLogger::replaceLoggers(QList<Logger*> newLoggers) {
+void MultiplexerLogger::replace_loggers(
+    QList<Logger*> &new_loggers, bool prepend_console,
+    Severity console_min_severity) {
+  if (prepend_console) {
+    QFile *console = new QFile;
+    console->open(1, QIODevice::WriteOnly|QIODevice::Unbuffered);
+    FileLogger *consoleLogger = new FileLogger(console, console_min_severity);
+    console->setParent(consoleLogger);
+    auto name = "Console"+consoleLogger->objectName();
+    consoleLogger->setObjectName(name);
+    new_loggers.prepend(consoleLogger);
+  }
   QMutexLocker locker(&_loggersMutex);
-  doReplaceLoggers(newLoggers);
-}
-
-void MultiplexerLogger::replaceLoggersPlusConsole(
-    Log::Severity consoleLoggerSeverity, QList<Logger*> newLoggers) {
-  QFile *console = new QFile;
-  console->open(1, QIODevice::WriteOnly|QIODevice::Unbuffered);
-  FileLogger *consoleLogger = new FileLogger(console, consoleLoggerSeverity);
-  newLoggers.prepend(consoleLogger);
-  QMutexLocker locker(&_loggersMutex);
-  doReplaceLoggers(newLoggers);
-}
-
-void MultiplexerLogger::doReplaceLoggers(QList<Logger*> newLoggers) {
   QList<Logger*> old_loggers(_loggers);
   old_loggers.detach();
   for (auto logger: old_loggers)
     if (logger->_auto_removable) {
-      if (!newLoggers.contains(logger))
+      if (!new_loggers.contains(logger))
         logger->shutdown();
       _loggers.removeAll(logger);
     }
-  for (auto logger: newLoggers) {
+  for (auto logger: new_loggers) {
     _loggers.append(logger);
   }
 }

log/multiplexerlogger.h
@@ -40,17 +40,12 @@ public:
   void addConsoleLogger(Severity severity, bool autoRemovable,
                         FILE *stream);
   void addQtLogger(Severity severity, bool autoRemovable);
-  /** Replace current auto-removable loggers with a new one.
-   * This method is thread-safe and switches loggers in an atomic way. */
-  void replaceLoggers(Logger *newLogger);
   /** Replace current auto-removable loggers with new ones.
+   *  Optionaly prepend a console logger.
    * This method is thread-safe and switches loggers in an atomic way. */
-  void replaceLoggers(QList<Logger*> newLoggers);
-  /** Replace current auto-removable loggers with new ones plus a new
-   * console logger with given logger severity.
-   * This method is thread-safe and switches loggers in an atomic way. */
-  void replaceLoggersPlusConsole(Severity consoleLoggerSeverity,
-                                 QList<Logger*> newLoggers);
+  void replace_loggers(
+      QList<Logger*> &new_loggers, bool prepend_console = false,
+      Severity console_min_severity = Fatal);
   QString pathToLastFullestLog();
   QStringList pathsToFullestLogs();
   QStringList pathsToAllLogs();
@@ -58,9 +53,6 @@ public:
 protected:
   void do_log(const Record &record) override;
   void do_shutdown() override;
-
-private:
-  inline void doReplaceLoggers(QList<Logger*> newLoggers);
 };
 
 } // ns p6::log